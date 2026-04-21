5ff8665074f07165558160de014e7354ebb84826
TobilobaOwoade <qudusowoade@gmail.com>
2026-04-21 16:36:05 -0400

    Enhance About section in feature branch

--- src/script.js
+++ src/script.js
@@ -5,3 +5,26 @@ function showMessage() {
 }
 
 showMessage();
+function enhanceAbout() {
+  const aboutSection = document.getElementById("about");
+
+  if (document.getElementById("extra-about")) return;
+
+  const newParagraph = document.createElement("p");
+  newParagraph.id = "extra-about";
+  newParagraph.textContent =
+    " I amToronto-based UI/UX designer dedicated to building intuitive and visually engaging digital products, focusing on user-centered design and meaningful experiences.";
+
+  aboutSection.appendChild(newParagraph);
+
+  showEnhancementMessage();
+}
+
+function showEnhancementMessage() {
+  const message = document.createElement("p");
+  message.id = "about-message";
+  message.textContent = "More information loaded";
+
+  document.getElementById("about").appendChild(message);
+
+}
